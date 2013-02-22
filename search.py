988d06bbeec7d2b05d5763d3e3120ce6cc4d5e5f
Alexander <alexchapurin@gmail.com>
2013-02-22 17:29:04 +0400

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,4 @@
-# Здесь надо реализовать dfs, bfs, ucs, A*. Первые два у меня получились. Выкладывать решения?
-
+# Здесь надо реализовать dfs, bfs, ucs, A*. Первые два у меня получились, я их выложил.
 """
 In search.py, you will implement generic search algorithms which are called
 by Pacman agents (in searchAgents.py).
@@ -76,15 +75,57 @@ def depthFirstSearch(problem):
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    route = [problem.getStartState()]    # "movement" vector     [ (),...,() ]
+    result = []   # result vector with solution
+    succs = {}    # copy of Successors
+    
+    while not problem.isGoalState(route[0]):
+    	l = len(route)
+		if not set([route[0]]).issubset(set(succs.keys())):
+			succs.update({route[0]:problem.getSuccessors(route[0])})
+		for dir in succs[route[0]]:   
+			if not set([dir[0]]).issubset(set(succs.keys())):
+				route.insert(0,dir[0])
+				result.append(dir[1])
+			if l != len(route): break
+		if l == len(route):
+			route.remove(route[0])
+			del result[len(result)-1]	
+    return result
 
 def breadthFirstSearch(problem):
     """
     Search the shallowest nodes in the search tree first.
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    route = [[problem.getStartState()]]    # "movement" vector, containing all possible directions!   [ [],[(),...,()],...[] ]  
+    result = []   # result vector with solution
+    succs = {}    # copy of Successors function
+    goal = 0      # flag for GoalState
+    
+    while goal == 0:
+		for path in route:
+			if not set([path[0]]).issubset(set(succs.keys())):
+				succs.update({path[0]:problem.getSuccessors(path[0])})
+				for i in range(len(succs[path[0]]) - 1):
+					var = path[:]
+					route.append(var)
+		x = []   # saving here all pathes in each level of tree
+		for path in route:
+			if goal == 1: break
+			l = len(path)
+			for dir in succs[path[0]]:
+				if not set([dir[0]]).issubset(set(succs.keys())) and not set([dir[0]]).issubset(set(x)):
+					path.insert(0,dir[0])
+					x.append(dir[0])
+				if problem.isGoalState(path[0]):
+					path.reverse()
+					for p in path:
+						result.append(p)
+					goal = 1
+					break
+				if l != len(path): 
+					break
+    return xyToDir(result)	 #this function is decribed below
 
 def uniformCostSearch(problem):
     "Search the node of least total cost first. "
@@ -102,6 +143,15 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     "Search the node that has the lowest combined cost and heuristic first."
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
+    
+def xyToDir(list):
+    answer = []
+	for i in range(len(list) - 1):
+		if list[i][0] - list[i+1][0] == 1: answer.append('West')
+		if list[i][0] - list[i+1][0] == -1: answer.append('East')
+		if list[i][1] - list[i+1][1] == 1: answer.append('South')
+		if list[i][1] - list[i+1][1] == -1: answer.append('North')
+	return answer
 
 
 # Abbreviations
